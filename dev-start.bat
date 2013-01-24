1cba59913e039b1869baabb4378904197b25489c
WangQing <ericwangqing@gmail.com>
2013-01-25 06:57:34 +0800

    更新了dev脚本，用于家里机器。

--- dev-start.bat
+++ dev-start.bat
@@ -1,4 +1,4 @@
-start "mongoDB" /D %MONGO_HOME%\bin mongod.exe --dbpath c:\mongo-data
+start "mongoDB" /D %MONGO_HOME%\bin mongod.exe --dbpath d:\mongodb-data
 start "node-inspector" node-inspector
 start "编译源代码" /D .\src lsc -wcbo . livescript
 start "YoYo服务器" supervisor src\server.js
