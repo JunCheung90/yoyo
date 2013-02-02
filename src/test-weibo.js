cd4fdb1a1fde467549920f19b5166ecb013acf4c
geraldasd <liurihiuasd@163.com>
2013-02-02 14:23:09 +0800

    修正模块调用的路径

--- src/test-weibo.js
+++ src/test-weibo.js
@@ -1,12 +1,11 @@
-var Weibo = require('../weibo').Weibo;
-var User = require('../weibo').User;
-var connect  = require('connect');
+var Weibo = require('./models/weibo').Weibo;
+var User = require('./models/weibo').User;
 
+//目前未用到appkey
 var app = new Weibo({
   app_key:  '2437764734',
   app_secret:  '468c0f5482ab4f8bf16cff51522207d4'
 });
-// app.listen(80);
 
 var user = new User({
   access_token: '2.00C5OL3DCybyeCdff214e680ICSURC',
@@ -14,25 +13,23 @@ var user = new User({
   screen_name: 'ucent_YoYo'	//昵称
 }, app);
 
-// 可以使用GET或POST来操作，直接根据 API 名称来调用
-user.get('2/statuses/user_timeline', {screen_name: '李开复', count: 1}, function (err, data) {
+// 微博API V2, http://open.weibo.com/wiki/API%E6%96%87%E6%A1%A3_V2
+var friends_timeline = user.api('GET', '2/statuses/friends_timeline');  //获取当前登录用户及其所关注用户的最新微博
+var user_timeline = user.api('GET', '2/statuses/user_timeline');  //获取用户发布的微博
+var user_show = user.api('GET', '2/users/show'); //获取用户信息
+
+// 当前用户最新发布的微博
+user_timeline({count: 1}, function (err, data) {
   if (err)
     console.log(err);
   else
     console.log(data);
 });
 
-user.post('2/statuses/repost', {id: 123456, status:'转发微博'}, function (err, data) {
-  // .....
-});
-
-// 也可以创建一个调用某API的函数（仅针对当前用户），并可设置默认参数
-var friends_timeline = user.api('GET', 'statuses/friends timeline', {count: 100});
-// 以后直接用 friends_timeline()即可
-friends_timeline(function (err, data) {
-  // ...
-});
-// 或者设置参数并调用
-friends_timeline({max_id: 1234445664}, function (err, data) {
-  // ...
-});
+// 无参数的调用
+// user_timeline(function (err, data) {
+//   if (err)
+//     console.log(err);
+//   else
+//     console.log(data);
+// });
